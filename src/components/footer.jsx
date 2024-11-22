f455d7b04c602e47a1251c621ecc7856b73ea359
Okunola Olamide <okunolaolubanjo@gmail.com>
2024-11-22 12:01:59 +0100

    Changed logo to ult

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -75,6 +75,7 @@ const ContactForm = () => {
 
         <div className="flex justify-center">
           <button
+          
             type="submit"
             disabled={isSubmitting}
             className={`bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 my-1 rounded-md w-full py-2 px-4 text-white font-semibold rounded-md ${isSubmitting ? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-600'} focus:outline-none`}
